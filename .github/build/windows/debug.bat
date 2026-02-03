9bb22f688214c555b4073fd51b51399047608fd8
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-03 07:51:33 +0200

    fix build scripts, remove linux for now and windows x64

--- .github/build/windows/debug.bat
+++ .github/build/windows/debug.bat
@@ -1,4 +1,3 @@
 for %%x in (SOF2MP) do (
     msbuild .\msvc\sof2gt_qmm.vcxproj /p:Configuration=Debug-%%x /p:Platform=x86
-    msbuild .\msvc\sof2gt_qmm.vcxproj /p:Configuration=Debug-%%x /p:Platform=x64
 )

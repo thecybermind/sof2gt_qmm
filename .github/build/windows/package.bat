af4e1d19df754b8c1f842c5a4ec539268fe1b348
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-11 11:47:48 +0200

    update variable name (does not conflict with any built-in variable, per ss64)

--- .github/build/windows/package.bat
+++ .github/build/windows/package.bat
@@ -4,7 +4,7 @@ del /q *
 rem copy ..\README.md .\
 rem copy ..\LICENSE .\
 
-for %%x in (SOF2MP) do (
-    copy ..\bin\Release-%%x\x86\sof2gt_qmm_%%x.dll .\  
+for %%G in (SOF2MP) do (
+    copy ..\bin\Release-%%G\x86\sof2gt_qmm_%%G.dll .\  
 )
 popd
